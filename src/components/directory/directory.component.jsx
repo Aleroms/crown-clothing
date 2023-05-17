371a09c902165392540814694bc4d7a2352d09a7
Santiago <santiagomorales333@yahoo.com>
2023-05-17 15:44:45 -0700

    idk >.<

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,10 +1,10 @@
-import CategoryItem from "../category-item/category-item.component";
+import DirectoryItem from "../directory-item/directory-item.component";
 import "./directory.style.scss";
 const Directory = ({ categories }) => {
   return (
     <div className="categories-container">
       {categories.map((cat) => (
-        <CategoryItem key={cat.id} category={cat} />
+        <DirectoryItem key={cat.id} category={cat} />
       ))}
     </div>
   );
