ac31aecbac610bb13f95de7037024cecf0edb32c
Santiago <santiagomorales333@yahoo.com>
2023-05-18 11:38:15 -0700

    converted cart-icon and directory-item into styled components

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,21 +1,20 @@
-import "./directory-item.styles.scss";
+import {
+  BackgroundImage,
+  Body,
+  DirectoryItemContainer,
+} from "./directory-item.styles.js";
 
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title } = category;
   console.log(category);
   return (
-    <div className="directory-container">
-      <div
-        className="background-image"
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-      />
-      <div className="body">
+    <DirectoryItemContainer>
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </div>
+      </Body>
+    </DirectoryItemContainer>
   );
 };
 export default DirectoryItem;
